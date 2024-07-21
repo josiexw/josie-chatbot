3851234948d76efd15bf7c4acb22b10376956bb3
josiexw <144638110+josiexw@users.noreply.github.com>
2024-07-21 15:38:54 -0400

    Update settings.py

--- josie_chatbot/settings.py
+++ josie_chatbot/settings.py
@@ -150,7 +150,9 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOWED_ORIGINS = ['https://josiexw.github.io/']
+CORS_ALLOWED_ORIGINS = ['https://josiexw.github.io/',]
+
+CORS_ALLOW_CREDENTIALS = True
 
 import dotenv
 import os
